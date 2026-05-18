2e5b175d25ba1143df06972059a660db676e64f5
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-18 14:52:24 -0300

    chore: update shareCode to TZCRS32VUKS

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=Z0O60LNNLFX&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=TZCRS32VUKS&afp=oa' });
     res.end();
   }
