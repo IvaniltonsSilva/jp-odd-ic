eba9e4c2f466e921ef22f9ca4d31c280e048a6df
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 10:41:07 -0300

    chore: shareCode 3XCU9UL2Q78 -> XU6LYIFTSPK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=3XCU9UL2Q78&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=XU6LYIFTSPK&afp=oa' });
     res.end();
   }
